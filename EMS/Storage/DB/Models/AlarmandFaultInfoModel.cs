05414c445e5f0b12066c56b8dc0ed1b9a1a61660
Edwin_Feng <zzzzyfwork@163.com>
2023-11-16 17:19:23 +0800

    231116 [FeatureStorage]将所有的储存Model写好了 PCSINFO，FaultandAlarm，ProtectParaSet，PCSControl

--- EMS/Storage/DB/Models/AlarmandFaultInfoModel.cs
+++ EMS/Storage/DB/Models/AlarmandFaultInfoModel.cs
@@ -13,8 +13,8 @@ namespace EMS.Storage.DB.Models
         public int number { get; set; }
         public string Device {get; set;}
         public int id { get; set; }
-        public string type {get; set; }
-        public string name { get; set;}
+        public string Module {get; set; }
+        public string Type{ get; set;}
         public int level { get; set; }
         public DateTime HappenTime { get; set; }
         
