fcf050aff2bd802b9dcd5cd7e516c9420cdc19cc
hxbao <hxbao2018@outlook.com>
2023-11-28 17:09:28 +0800

    20231128 [feature]添加单元测试框架项目到解决方案

---
+++ TestEMS/ApiTest/PcsApiTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EMS.Api;
+
+namespace TestEMS.ApiTest
+{
+    [TestClass]
+    public class PcsApiTest
+    {
+        [TestMethod]
+        public void TestPcsSetAction()
+        {
+            
+            Assert.AreEqual(true, PcsApi.PcsSetAction(1, 1, null));
+        }
+    }
+}
