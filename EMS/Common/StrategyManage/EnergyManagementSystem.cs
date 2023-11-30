d9c49a62af3d47362791074c8818b684ffe1e4f4
Yingzhong Gu <gyzdmgqy@msn.com>
2023-11-30 14:31:02 +0800

    更新系统框架，加入静态类，融合公共api

--- EMS/Common/StrategyManage/EnergyManagementSystem.cs
+++ EMS/Common/StrategyManage/EnergyManagementSystem.cs
@@ -1,4 +1,5 @@
-﻿using EMS.Common.StrategyManage;
+﻿using EMS.Api
+using EMS.Common.StrategyManage;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,15 +9,45 @@ using System.Threading.Tasks;
 
 namespace EMS.Model
 {
+
+    public class SmartMeter {
+        private double _voltage;
+        public double Voltage { get; set; }
+
+        public void readVoltage() {}
+
+        public double GetVoltage() { return _voltage; }
+    }
+
+    public class SmartMeterManager {
+        private List<object> smart_meters;
+    }
     public class EnergyManagementSystem
     {
         private Thread _operationThread;
         private EmsController _controller;
+        private object _pcs_manager;
+        private object _smart_meter_manager;
+        private object _bms_manager;
+        private object _database_manager;
+        private object _cloud_manager;
+
+        
+
+        private static EnergyManagementSystem _globalInstance;
+
+        public static EnergyManagementSystem GlobalInstance { get { return _globalInstance; } }
+
+        public object PcsManager {  get { return _pcs_manager; } }
         public EnergyManagementSystem()
         {
             _controller = new EmsController();
             _operationThread = null;
 
+        }
+
+        public void Initialization(object _pcs_manager, object _smart_meter_manager, object _bms_manager, object _database_manager, object _cloud_manager) {
+            return;
         }
 
         public void RestartOperationThread()
