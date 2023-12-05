b36e10202cc8a8a66008c8a93a372e051792bdbf
Yingzhong Gu <gyzdmgqy@msn.com>
2023-12-05 16:31:17 +0800

    Fix some minor issues.

--- EMS/ViewModel/ParameterSettingViewModel.cs
+++ EMS/ViewModel/ParameterSettingViewModel.cs
@@ -558,11 +558,6 @@ namespace EMS.ViewModel
         
         private void SyncClusterVolThreshInfo()
         {
-            List<double> data = new List<double>();
-            data = BmsApi.GetTotalSOC();
-            double avgsoc = BmsApi.GetAvgSOC();
-            double minsoc = BmsApi.GetMinSOC();
-
             ModbusClient.WriteFunc(40200, (ushort)(ClusterVolUpLimitLv1 * 10));
             ModbusClient.WriteFunc(40201, (ushort)(ClusterVolUpLimitLv2 * 10));
             ModbusClient.WriteFunc(40202, (ushort)(ClusterVolUpLimitLv3 * 10));
