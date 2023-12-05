508fa3ea5c6050681c9a5a33624eb0849905b0d3
Edwin_Feng <zzzzyfwork@163.com>
2023-12-05 09:59:06 +0800

    20231205 添加了SOCAPI

--- EMS/Api/BmsApi.cs
+++ EMS/Api/BmsApi.cs
@@ -10,11 +10,15 @@ namespace EMS.Api
 
     public class BmsApi
     {
-        public static List<BatteryTotalBase> GetBMSTotalInfo()
+        /// <summary>
+        /// 得到BMS信息
+        /// </summary>
+        /// <returns></returns>
+        public static List<BatteryTotalBase> GetBMSTotalInfo()//得到所有BMS信息
         {
             return EnergyManagementSystem.GlobalInstance.BmsManager.BmsTotalList;
         }
-        public static BatteryTotalBase GetBMSTotalInfo(string bcmuid)
+        public static BatteryTotalBase GetBMSTotalInfo(string bcmuid)//得到单簇信息
         {
             List<BatteryTotalBase>totallist= EnergyManagementSystem.GlobalInstance.BmsManager.BmsTotalList;
             foreach(var total in totallist)
@@ -28,6 +32,10 @@ namespace EMS.Api
             }
             return null;
         }
+        /// <summary>
+        /// 得到BMS所有告警故障信息
+        /// </summary>
+        /// <returns></returns>
         public static List<string> GetTotalAlarmInfo()
         {
             List<BatteryTotalBase> totallist = EnergyManagementSystem.GlobalInstance.BmsManager.BmsTotalList;//获取所有电池数据
@@ -54,9 +62,47 @@ namespace EMS.Api
             totalalarminfo=totalalarminfo.Distinct().ToList();
             return totalalarminfo;
         }
+        /// <summary>
+        /// 所有SOC、最大最小平均SOC
+        /// </summary>
+        /// <returns></returns>
+        public static List<double> GetTotalSOC()
+        {
+            List<BatteryTotalBase> batteryTotalBases = new List<BatteryTotalBase>();
+            batteryTotalBases = EnergyManagementSystem.GlobalInstance.BmsManager.BmsTotalList;
+            List<double> SOCTotalList = new List<double>();
+            foreach (var total in batteryTotalBases)
+            {
+                SOCTotalList.Add(total.TotalSOC);
+            }
+            return SOCTotalList;
 
 
-        
+        }
 
+        public static double GetMinSOC()
+        {
+            List<double> sOCTotalList = new List<double>();
+            sOCTotalList = GetTotalSOC();
+            double minSOC = sOCTotalList.Min();
+            return minSOC;
+        }
+
+        public static double GetMaxSOC()
+        {      
+            List<double> sOCTotalList = new List<double>();
+            sOCTotalList = GetTotalSOC();
+            double maxSOC = sOCTotalList.Max();
+            return maxSOC;
+        }
+        public static double GetAvgSOC()
+        {
+            List<double> sOCTotalList = new List<double>();
+            sOCTotalList = GetTotalSOC();
+            double avgSOC = sOCTotalList.Average();
+            return avgSOC;
+        }
+
+      
     }
 }
