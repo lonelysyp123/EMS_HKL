94021baefc5a0d3539da20a9f5dc5ab0950edf50
lonelysyp123 <624273987@qq.com>
2023-12-11 08:31:35 +0800

    2023/12/11 [feat] 电表管理员

--- EMS/Common/StrategyManage/EnergyManagementSystem.cs
+++ EMS/Common/StrategyManage/EnergyManagementSystem.cs
@@ -12,22 +12,30 @@ using System.Threading.Tasks;
 
 namespace EMS.Model
 {
+    public class SmartMeterManager 
+    {
+        private List<ElectricityMeterModel> _smartMeters;
+        public List<ElectricityMeterModel> SmartMeters { get { return _smartMeters; } } //封装，不能set
 
-    public class SmartMeter {
-        private double _voltage;
-        public double Voltage { get; set; }
+        public void AddDev(ElectricityMeterModel item)
+        {
+            if (_smartMeters == null)
+            {
+                _smartMeters = new List<ElectricityMeterModel>();
+            }
+            _smartMeters.Add(item);
+        }
 
-        public void readVoltage() {}
-
-        public double GetVoltage() { return _voltage; }
-    }
-  
-
-       
-
-
-    public class SmartMeterManager {
-        private List<object> smart_meters;
+        public void RemoveDev(ElectricityMeterModel item)
+        {
+            if (_smartMeters != null && _smartMeters.Count > 0)
+            {
+                if (_smartMeters.Contains(item))
+                {
+                    _smartMeters.Remove(item);
+                }
+            }
+        }
     }
 
     public class BmsManager
@@ -53,7 +61,7 @@ namespace EMS.Model
         private Thread _operationThread;
         private EmsController _controller;
         private object _pcs_manager;
-        private object _smart_meter_manager;
+        private SmartMeterManager _smart_meter_manager;
         private BmsManager _bms_manager;
         private object _database_manager;
         private object _cloud_manager;
@@ -69,14 +77,15 @@ namespace EMS.Model
             _globalInstance = new EnergyManagementSystem();
         }
 
-       public BmsManager BmsManager { get { return _bms_manager; } }
+        public BmsManager BmsManager { get { return _bms_manager; } }
         public object PcsManager {  get { return _pcs_manager; } }
+        public SmartMeterManager SmartMeterManager { get { return _smart_meter_manager; } }
         public EnergyManagementSystem()
         {
             _controller = new EmsController();
             _operationThread = null;
             _bms_manager = new BmsManager();
-
+            _smart_meter_manager = new SmartMeterManager();
         }
        
         public void Initialization(object _pcs_manager, object _smart_meter_manager,  object _database_manager, object _cloud_manager) 
