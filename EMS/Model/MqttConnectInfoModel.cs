0f70892824e683ecfe161a377a9779188498c5cf
TNCN <cf10082007@qq.com>
2023-12-18 11:06:00 +0800

    20231218 [feat] 新增mqtt client service和单元测试

---
+++ EMS/Model/MqttConnectInfoModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TNCN.EMS.Model
+{
+    public class MqttConnectInfoModel
+    {
+        /// <summary>
+        /// ip地址
+        /// </summary>
+        public string Ip { get { return ip; } }
+        private string ip;
+        /// <summary>
+        /// 端口号
+        /// </summary>
+        public int Port { get { return port; } }
+        private int port;
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        public string UserName { get { return userName; } }
+        private string userName;
+        /// <summary>
+        /// 密码
+        /// </summary>
+        public string Password { get { return password; } }
+        private string password;
+        /// <summary>
+        /// 客户端id
+        /// </summary>
+        public string ClientId { get { return clientId; } }
+        private string clientId;
+
+        public int KeepAlivePeriod { get { return keepAlivePeriod; } }
+        private int keepAlivePeriod;
+        /// <summary>
+        /// 订阅的topic列表
+        /// </summary>
+        public List<string> Topics { get; set; }
+
+        public MqttConnectInfoModel(string ip, int port, string userName, string password, string clientId, int keepAlivePeriod) { 
+            this.ip = ip;
+            this.port = port;
+            this.userName = userName;
+            this.password = password;
+            this.clientId = clientId;
+            this.keepAlivePeriod = keepAlivePeriod;
+        }
+    }
+}
---
+++ TestEMS/ApiTest/MqttTest.cs
@@ -0,0 +1,29 @@
+﻿using TNCN.EMS.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TNCN.EMS.Model;
+
+namespace TestEMS.ApiTest
+{
+    [TestClass]
+    public class MqttTest
+    {
+        [TestMethod]
+        public void TestMqttClient() {
+            List<string> topics = new List<string>();
+            topics.Add("/tncn/test/cf/thing/event/property/post_reply");
+            topics.Add("/tncn/test/cf/thing/service/property/set");
+            
+            MqttConnectInfoModel mqttConnectInfo = new MqttConnectInfoModel("116.62.159.155", 1883, "admin", "zhny2020", "cftest", 55);
+            mqttConnectInfo.Topics = topics;
+            MqttClientService mqttClientService = new MqttClientService();
+            mqttClientService.StartMqttClient(mqttConnectInfo);
+            bool result = mqttClientService.Publish("/tncn/test/cf/thing/event/property/post", Encoding.UTF8.GetBytes("cftest"));
+            Assert.IsTrue(result);
+        }
+
+    }
+}
