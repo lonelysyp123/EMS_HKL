94d7f77cd8194515a2d48b322cfb5f866e705561
momowujing <2384924585@qq.com>
2023-12-22 15:20:40 +0800

    监控界面—BMS前端界面完成。

--- EMS/View/NewEMSView/Monitor_BMSPage.xaml.cs
+++ EMS/View/NewEMSView/Monitor_BMSPage.xaml.cs
@@ -24,5 +24,10 @@ namespace EMS.View.NewEMSView
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
