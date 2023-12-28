cac28387212449c41381b50b2dee5a60302416f7
尹培 宋 <624273987@qq.com>
2023-12-28 13:54:57 +0800

    [feat] 电表数据服务

---
+++ EMS/Service/SmartMeterDataService.cs
@@ -0,0 +1,53 @@
+﻿using Modbus.Device;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace EMS.Service
+{
+    public class SmartMeterDataService
+    {
+        private bool _isConnected;
+        public bool IsConnected
+        {
+            get => _isConnected;
+            private set
+            {
+                if (_isConnected != value)
+                {
+                    _isConnected = value;
+                    OnChangeState(_isConnected, _isDaqData);
+                }
+            }
+        }
+
+        private bool _isDaqData;
+        public bool IsDaqData
+        {
+            get => _isDaqData;
+            private set
+            {
+                if (_isDaqData != value)
+                {
+                    _isDaqData = value;
+                    OnChangeState(_isConnected, _isDaqData);
+                }
+            }
+        }
+
+        private string IP;
+        private int Port;
+        private TcpClient _client;
+        private ModbusMaster _master;
+        private Action<bool, bool> OnChangeState;
+
+        public SmartMeterDataService()
+        {
+
+        }
+    }
+}
