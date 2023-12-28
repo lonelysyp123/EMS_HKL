624c2b265f8534dbfd41a3720a5e2359293bda67
尹培 宋 <624273987@qq.com>
2023-12-28 16:48:15 +0800

    [feat] 电表模块重构

--- EMS/Service/SmartMeterDataService.cs
+++ EMS/Service/SmartMeterDataService.cs
@@ -1,11 +1,18 @@
-﻿using Modbus.Device;
+﻿using EMS.Common;
+using EMS.Model;
+using EMS.ViewModel;
+using Modbus.Device;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
+using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Web.UI.WebControls.WebParts;
 
 namespace EMS.Service
 {
@@ -39,15 +46,257 @@ namespace EMS.Service
             }
         }
 
-        private string IP;
-        private int Port;
-        private TcpClient _client;
-        private ModbusMaster _master;
         private Action<bool, bool> OnChangeState;
+        private SerialPort SerialPortInstance;
+        public Configuaration Configuaration;
 
         public SmartMeterDataService()
         {
+            smartMeterModels = new BlockingCollection<SmartMeterModel>(new ConcurrentQueue<SmartMeterModel>(),300);
+        }
 
+        public void RegisterState(Action<bool, bool> action)
+        {
+            OnChangeState = action;
+        }
+
+        public void SetCommunicationConfig(Configuaration configuaration)
+        {
+            Configuaration = configuaration;
+        }
+
+        public bool Connect()
+        {
+            if (!IsConnected)
+            {
+                if (Open())
+                {
+                    IsConnected = true;
+                    return true;
+                }
+
+                // 采集状态
+                if (IsDaqData)
+                {
+                    StartDaqData();
+                }
+                return true;
+            }
+            return false;
+        }
+
+        public bool Disconnect()
+        {
+            if (IsConnected)
+            {
+                if (IsDaqData)
+                {
+                    StopDaqData();
+                }
+
+                if (SerialPortInstance != null)
+                {
+                    if (SerialPortInstance.IsOpen)
+                    {
+                        SerialPortInstance.Close();
+                    }
+                }
+                IsConnected = false;
+            }
+            return true;
+        }
+
+        public void StopDaqData()
+        {
+            IsDaqData = false;
+        }
+
+        public void StartDaqData()
+        {
+            if (IsConnected)
+            {
+                if (!IsDaqData)
+                {
+                    IsDaqData = true;
+                    Thread th = new Thread(DaqDataTh);
+                    th.IsBackground = true;
+                    th.Start();
+                }
+            }
+        }
+
+        private bool Open()
+        {
+            try
+            {
+                SerialPortInstance = new SerialPort();
+                SerialPortInstance.PortName = Configuaration.SelectedCommPort;
+                SerialPortInstance.BaudRate = Configuaration.SelectedBaudRate;
+                SerialPortInstance.DataBits = Configuaration.SelectedDataBits;
+                SerialPortInstance.StopBits = Configuaration.SelectedStopBits;
+                SerialPortInstance.Parity = Configuaration.SelectedParity;
+                SerialPortInstance.ReadTimeout = 100;
+                SerialPortInstance.Open();
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public void SetDaqTimeSpan(int value)
+        {
+            DaqTimeSpan = value;
+        }
+
+        private int DaqTimeSpan = 1;
+        public BlockingCollection<SmartMeterModel> smartMeterModels;
+        // CS校验不计算0xFE, 0xFE, 0xFE, 0xFE
+        byte[] Request_GetVoltage_A = new byte[] { 0x68, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x68, 0x11, 0x04, 0x02, 0x01, 0x01, 0x00, 0xA4, 0x16 };
+        byte[] Response_GetVoltage_A = new byte[] { 0xFE, 0xFE, 0xFE, 0xFE, 0x68, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x68, 0x91, 0x08, 0x33, 0x33, 0x33, 0x33, 0x00, 0x00, 0x00, 0x00, 0x44, 0x16 };
+        byte[] Request_GetVoltage_B = new byte[0];
+        byte[] Response_GetVoltage_B = new byte[0];
+        private void DaqDataTh()
+        {
+            while (IsConnected && IsDaqData)
+            {
+                try
+                {
+                    Thread.Sleep(DaqTimeSpan * 500);
+                    // 采集数据
+                    SmartMeterModel model = new SmartMeterModel();
+                    var Data_Voltage_A = ReadDataForCmd(Request_GetVoltage_A, Response_GetVoltage_A.Length);
+                    if(DataDecode(Data_Voltage_A, Response_GetVoltage_A, out int Voltage_A))
+                    {
+                        model.Voltage_A = Voltage_A;
+                    }
+
+                    var Data_Voltage_B = ReadDataForCmd(Request_GetVoltage_B, Response_GetVoltage_B.Length);
+                    if (DataDecode(Data_Voltage_A, Response_GetVoltage_A, out int Voltage_B))
+                    {
+                        model.Voltage_B = Voltage_B;
+                    }
+                    smartMeterModels.TryAdd(model);
+                }
+                catch (Exception)
+                {
+                    break;
+                }
+            }
+        }
+
+        private byte[] ReadDataForCmd(byte[] Request, int num)
+        {
+            try
+            {
+                var readBytes = new byte[num];
+                SerialPortInstance.Write(Request, 0, Request.Length);
+                SerialPortInstance.Read(readBytes, 0, num);
+                return readBytes;
+            }
+            catch (InvalidOperationException)
+            {
+                //串口断开，尝试重连
+                if (!SerialPortInstance.IsOpen && !IsCommunicationProtectState)
+                {
+                    if (CommunicationCheck())
+                    {
+                        var readBytes = new byte[num];
+                        SerialPortInstance.Write(Request, 0, Request.Length);
+                        SerialPortInstance.Read(readBytes, 0, num);
+                        return readBytes;
+                    }
+                }
+            }
+            return new byte[num];
+        }
+
+        private static int reconnectInterval = 150; // ms
+        private static int maxReconnectTimes = 3;
+        private int reconnectCount = 0;
+        private bool IsCommunicationProtectState = false;
+        private Thread CommunicationProtectTr;
+        private bool CommunicationCheck()
+        {
+            while (true)
+            {
+                try
+                {
+                    SerialPortInstance.Open();
+                    reconnectCount = 0;
+                    return true;
+                }
+                catch (Exception)
+                {
+                    reconnectCount++;
+                    if (reconnectCount > maxReconnectTimes)
+                    {
+                        IsCommunicationProtectState = true;
+                        IsConnected = false;
+                        CommunicationProtectTr.Start();
+                        return false;
+                    }
+                }
+            }
+        }
+
+        private static int reconnectIntervalLong = 60 * 1000 * 5; // ms
+        private void CommunicationProtect()
+        {
+            while (!IsConnected)
+            {
+                Thread.Sleep(reconnectIntervalLong);
+                try
+                {
+                    SerialPortInstance.Open();
+                    IsConnected = true;
+                    LogUtils.Debug("电表保护机制重连成功，设备上线");
+                }
+                catch (Exception)
+                {
+                    LogUtils.Debug("电表保护机制重连失败，继续尝试重新链接");
+                    continue;
+                }
+            }
+        }
+
+        private bool DataDecode(byte[] data, byte[] model, out int value)
+        {
+            if (CheckData(model, data))
+            {
+                byte[] bf = new byte[] { data[data.Length - 6], data[data.Length - 5], data[data.Length - 4], data[data.Length-3] };
+                value =  BitConverter.ToInt32(bf, 0);
+                return true;
+            }
+            value = 0;
+            return false;
+        }
+
+        /// <summary>
+        /// 校验数据
+        /// </summary>
+        /// <param name="standardVBytes">标准模板</param>
+        /// <param name="readBytes">读取到的数据</param>
+        /// <returns>校验结果</returns>
+        private bool CheckData(byte[] standardBytes, byte[] readBytes)
+        {
+            if (standardBytes.Length == readBytes.Length)
+            {
+                for (int i = 0; i < standardBytes.Length - 4; i++)
+                {
+                    if (standardBytes[i] != readBytes[i])
+                    {
+                        return false;
+                    }
+                }
+            }
+            else
+            {
+                return false;
+            }
+            return true;
         }
     }
 }
--- EMS/ViewModel/ElectricMeterViewModel.cs
+++ EMS/ViewModel/ElectricMeterViewModel.cs
@@ -1,8 +1,11 @@
 ﻿using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
+using EMS.Common;
 using EMS.Model;
+using EMS.Service;
 using log4net;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO.Ports;
@@ -11,109 +14,367 @@ using System.Net;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Web.UI.WebControls.WebParts;
+using System.Windows.Media.Imaging;
 
 namespace EMS.ViewModel
 {
     public class ElectricMeterViewModel : ViewModelBase
     {
-        public RelayCommand OpenSerialPortCommand { get; set; }
-        public RelayCommand CloseSerialPortCommand { get; set; }
-        public RelayCommand StartDaqDataCommand { get; set; }
-        public RelayCommand StopDaqDataCommand { get; set; }
+        #region ObservableObject
 
-        public ElectricityMeterModel electricityMeterModel;
-        private ILog Logger;
+        private int _voltage_A;
+        public int Voltage_A
+        {
+            get => _voltage_A;
+            set
+            {
+                SetProperty(ref _voltage_A, value);
+            }
+        }
+
+        private int _voltage_B;
+        public int Voltage_B
+        {
+            get => _voltage_B;
+            set
+            {
+                SetProperty(ref _voltage_B, value);
+            }
+        }
+
+        private int _voltage_C;
+        public int Voltage_C
+        {
+            get => _voltage_C;
+            set
+            {
+                SetProperty(ref _voltage_C, value);
+            }
+        }
+
+        private int _electric_A;
+        public int Electric_A
+        {
+            get => _electric_A;
+            set
+            {
+                SetProperty(ref _electric_A, value);
+            }
+        }
+
+        private int _electric_B;
+        public int Electric_B
+        {
+            get => _electric_B;
+            set
+            {
+                SetProperty(ref _electric_B, value);
+            }
+        }
+
+        private int _electric_C;
+        public int Electric_C
+        {
+            get => _electric_C;
+            set
+            {
+                SetProperty(ref _electric_C, value);
+            }
+        }
+
+        private int _activePower_A;
+        public int ActivePower_A
+        {
+            get => _activePower_A;
+            set
+            {
+                SetProperty(ref _activePower_A, value);
+            }
+        }
+
+        private int _activePower_B;
+        public int ActivePower_B
+        {
+            get => _activePower_B;
+            set
+            {
+                SetProperty(ref _activePower_B, value);
+            }
+        }
+
+        private int _activePower_C;
+        public int ActivePower_C
+        {
+            get => _activePower_C;
+            set
+            {
+                SetProperty(ref _activePower_C, value);
+            }
+        }
+
+        private int _activePower_Total;
+        public int ActivePower_Total
+        {
+            get => _activePower_Total;
+            set
+            {
+                SetProperty(ref _activePower_Total, value);
+            }
+        }
+
+        private int _reactivePower_A;
+        public int ReactivePower_A
+        {
+            get => _reactivePower_A;
+            set
+            {
+                SetProperty(ref _reactivePower_A, value);
+            }
+        }
+
+        private int _reactivePower_B;
+        public int ReactivePower_B
+        {
+            get => _reactivePower_B;
+            set
+            {
+                SetProperty(ref _reactivePower_B, value);
+            }
+        }
+
+        private int _reactivePower_C;
+        public int ReactivePower_C
+        {
+            get => _reactivePower_C;
+            set
+            {
+                SetProperty(ref _reactivePower_C, value);
+            }
+        }
+
+        private int _reactivePower_Total;
+        public int ReactivePower_Total
+        {
+            get => _reactivePower_Total;
+            set
+            {
+                SetProperty(ref _reactivePower_Total, value);
+            }
+        }
+
+        public Configuaration Configuaration { get; set; }
+
+        private bool _isConnected;
+        public bool IsConnected
+        {
+            get { return _isConnected; }
+            private set
+            {
+                if (_isConnected != value)
+                {
+                    _isConnected = value;
+                }
+            }
+        }
+
+        private bool _isDaqData;
+        public bool IsDaqData
+        {
+            get => _isDaqData;
+            private set
+            {
+                if (_isDaqData != value)
+                {
+                    _isDaqData = value;
+                }
+            }
+        }
+
+        private bool _isRecordData = false;
+        public bool IsRecordData
+        {
+            get => _isRecordData;
+            private set
+            {
+                if (_isRecordData != value)
+                {
+                    _isRecordData = value;
+                }
+            }
+        }
+
+        #endregion
+
+        public SmartMeterModel CurrentSmartMeterModel;
+        private BlockingCollection<SmartMeterModel> SmartMeterModelList;
+        private SmartMeterDataService service;
 
         public ElectricMeterViewModel()
         {
-            electricityMeterModel = new ElectricityMeterModel();
-
-            OpenSerialPortCommand = new RelayCommand(OpenSerialPort);
-            CloseSerialPortCommand = new RelayCommand(CloseSerialPort);
-            StartDaqDataCommand = new RelayCommand(StartDaqData);
-            StopDaqDataCommand = new RelayCommand(StopDaqData);
-
-            Logger = LogManager.GetLogger(typeof(ElectricityMeterModel));
+            service = new SmartMeterDataService();
+            service.RegisterState(ServiceStateCallBack);
         }
 
+        private void ServiceStateCallBack(bool isConnected, bool isDaqData)
+        {
+            App.Current.Dispatcher.Invoke(() => {
+                IsConnected = isConnected;
+                IsDaqData = isDaqData;
+            });
+        }
+
+        [RelayCommand]
         private void StopDaqData()
         {
-            electricityMeterModel.StartDaqTh();
+            service.StopDaqData();
         }
 
+        [RelayCommand]
         private void StartDaqData()
         {
-            electricityMeterModel.StopDaqTh();
+            service.StartDaqData();
+            Thread thread = new Thread(RefreshDataTh);
+            thread.IsBackground = true;
+            thread.Start();
         }
 
-        private void CloseSerialPort()
+        private void RefreshDataTh()
         {
-            if (electricityMeterModel.Close())
+            while (IsDaqData)
             {
-                Logger.Info("Close Serial Port("+ electricityMeterModel.Configuaration.SelectedCommPort + ")");
-            }
-            else
-            {
-                Logger.Info("Close Serial Port(" + electricityMeterModel.Configuaration.SelectedCommPort + ") Failed");
+                if (service.smartMeterModels.TryTake(out SmartMeterModel CurrentSmartMeterModel))
+                {
+                    var model = (SmartMeterModel)CurrentSmartMeterModel.Clone();
+                    SmartMeterModelList.Add(model);
+                    // 把数据分发给需要显示的内容
+                    App.Current.Dispatcher.Invoke(() =>
+                    {
+                        RefreshData(CurrentSmartMeterModel);
+                    });
+
+                    if (IsRecordData)
+                    {
+                        SaveData(CurrentSmartMeterModel);
+                    }
+                }
+                else
+                {
+                    Thread.Sleep(500);
+                }
             }
         }
 
+        private void SaveData(SmartMeterModel currentSmartMeterModel)
+        {
+            // TODO
+        }
+
+        private void RefreshData(SmartMeterModel currentSmartMeterModel)
+        {
+            this.Voltage_A = currentSmartMeterModel.Voltage_A;
+            this.Voltage_B = currentSmartMeterModel.Voltage_B;
+            this.Voltage_C = currentSmartMeterModel.Voltage_A;
+            this.Electric_A = currentSmartMeterModel.Electric_A;
+            this.Electric_B = currentSmartMeterModel.Electric_B;
+            this.Electric_C = currentSmartMeterModel.Electric_C;
+            this.ActivePower_A = currentSmartMeterModel.ActivePower_A;
+            this.ActivePower_B = currentSmartMeterModel.ActivePower_B;
+            this.ActivePower_C = currentSmartMeterModel.ActivePower_C;
+            this.ActivePower_Total = currentSmartMeterModel.ActivePower_Total;
+            this.ReactivePower_A = currentSmartMeterModel.ReactivePower_A;
+            this.ReactivePower_B = currentSmartMeterModel.ReactivePower_B;
+            this.ReactivePower_C = currentSmartMeterModel.ReactivePower_C;
+            this.ReactivePower_Total = currentSmartMeterModel.ReactivePower_Total;
+        }
+
+        [RelayCommand]
+        public void CloseSerialPort()
+        {
+            service.Disconnect();
+        }
+
+        [RelayCommand]
         private void OpenSerialPort()
         {
-            if (electricityMeterModel.Open())
-            {
-                Logger.Info("Open Serial Port(" + electricityMeterModel.Configuaration.SelectedCommPort + ")");
-            }
-            else
-            {
-                Logger.Info("Open Serial Port(" + electricityMeterModel.Configuaration.SelectedCommPort + ") Failed");
-            }
+            service.SetCommunicationConfig(Configuaration);
+            service.Connect();
         }
 
         public ThreePhaseValue GetThreePhaseVoltage()
         {
             var item = new ThreePhaseValue();
-            item.PhaseA = electricityMeterModel.Voltage_A;
-            item.PhaseB = electricityMeterModel.Voltage_B;
-            item.PhaseC = electricityMeterModel.Voltage_C;
+            item.PhaseA = CurrentSmartMeterModel.Voltage_A;
+            item.PhaseB = CurrentSmartMeterModel.Voltage_B;
+            item.PhaseC = CurrentSmartMeterModel.Voltage_C;
             return item;
         }
 
         public ThreePhaseValue GetThreePhaseElectric()
         {
             var item = new ThreePhaseValue();
-            item.PhaseA = electricityMeterModel.Electric_A;
-            item.PhaseB = electricityMeterModel.Electric_B;
-            item.PhaseC = electricityMeterModel.Electric_C;
+            item.PhaseA = CurrentSmartMeterModel.Electric_A;
+            item.PhaseB = CurrentSmartMeterModel.Electric_B;
+            item.PhaseC = CurrentSmartMeterModel.Electric_C;
             return item;
         }
 
         public ThreePhaseValue GetThreePhaseActivePower()
         {
             var item = new ThreePhaseValue();
-            item.PhaseA = electricityMeterModel.ActivePower_A;
-            item.PhaseB = electricityMeterModel.ActivePower_B;
-            item.PhaseC = electricityMeterModel.ActivePower_C;
+            item.PhaseA = CurrentSmartMeterModel.ActivePower_A;
+            item.PhaseB = CurrentSmartMeterModel.ActivePower_B;
+            item.PhaseC = CurrentSmartMeterModel.ActivePower_C;
             return item;
         }
 
         public ThreePhaseValue GetThreePhaseReactivePower()
         {
             var item = new ThreePhaseValue();
-            item.PhaseA = electricityMeterModel.ReactivePower_A;
-            item.PhaseB = electricityMeterModel.ReactivePower_B;
-            item.PhaseC = electricityMeterModel.ReactivePower_C;
+            item.PhaseA = CurrentSmartMeterModel.ReactivePower_A;
+            item.PhaseB = CurrentSmartMeterModel.ReactivePower_B;
+            item.PhaseC = CurrentSmartMeterModel.ReactivePower_C;
             return item;
         }
 
         public double GetRealPowerTotal()
         {
-            return electricityMeterModel.ActivePower_Total;
+            return CurrentSmartMeterModel.ActivePower_Total;
         }
 
         public double GetReactivePowerTotal()
         {
-            return electricityMeterModel.ReactivePower_Total;
+            return CurrentSmartMeterModel.ReactivePower_Total;
+        }
+    }
+
+    public struct ThreePhaseValue
+    {
+        public double PhaseA;
+        public double PhaseB;
+        public double PhaseC;
+    }
+
+    public class Configuaration
+    {
+        public List<SerialPortSettingsModel> CommPorts { get; private set; }
+        public string SelectedCommPort { get; set; }
+        public List<SerialPortSettingsModel> BaudRates { get; private set; }
+        public int SelectedBaudRate { get; set; }
+        public List<SerialPortSettingsModel> Parities { get; private set; }
+        public Parity SelectedParity { get; set; }
+        public List<SerialPortSettingsModel> StopBitsList { get; private set; }
+        public StopBits SelectedStopBits { get; set; }
+        public int[] DataBits { get; private set; }
+        public int SelectedDataBits { get; set; }
+
+        public Configuaration()
+        {
+            CommPorts = SerialPortSettingsModel.Instance.getCommPorts();
+            BaudRates = SerialPortSettingsModel.Instance.getBaudRates();
+            Parities = SerialPortSettingsModel.Instance.getParities();
+            StopBitsList = SerialPortSettingsModel.Instance.getStopBits();
+            DataBits = new int[] { 4, 5, 6, 7, 8 };
         }
     }
 }
